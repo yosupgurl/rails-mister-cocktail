ee55ad050c3f974789330c64dba44acffbdacc20
Ben Abitbul <benabitbul@hotmail.com>
2018-06-03 18:30:27 +0300

    fixed heroku issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :cocktails, only: [:show, :index, :new, :create, :destroy] do
    resources :doses, only: [:create, :new, :index, :destroy]
+   resources :cocktails, except: :index
   end
-  root to: "cocktails#index"
+  root to: 'cocktails#index'
   delete 'doses/:id', to: "doses#destroy", as: :destroy_dose
   delete 'cocktails/:id', to: "cocktails#destroy", as: :destroy_cocktail
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
